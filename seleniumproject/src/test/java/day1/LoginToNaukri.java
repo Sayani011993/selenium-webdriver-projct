c8f812842311f393d86d9a9dbcb2c9c59d7ea455
Sayani011993 <mitrasayani71@gmail.com>
2023-07-19 17:40:52 +0530

    Committted formatted code

--- seleniumproject/src/test/java/day1/LoginToNaukri.java
+++ seleniumproject/src/test/java/day1/LoginToNaukri.java
@@ -1,10 +1,7 @@
 package day1;
-// From New Branch
-import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginToNaukri {
 
